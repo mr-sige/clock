3e5bf9d953ad95b587b98c4083a4ecef0fb4a34c
sige <sige@configcat.com>
2019-09-16 13:39:02 +0200

    dotnet app

---
+++ dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+namespace Clock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var today = DateTime.Today.ToString("yyyy.MM.");
+                var time = DateTime.Now.ToString("HH: mm: ss");
+                Console.Clear();
+                Console.WriteLine("{0} {1}", today, time);
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
